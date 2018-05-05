f05ea6f38e5f4b6cb1820ad088b5fb08c205f483
Daniel Spiewak <djspiewak@gmail.com>
2018-05-05 11:16:34 -0600

    Added sonatype variant

---
+++ sonatype/src/main/scala/sbtspiewak/SpiewakSonatypePlugin.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 Daniel Spiewak
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sbtspiewak
+
+import sbt._, Keys._
+
+import xerial.sbt.Sonatype, Sonatype.autoImport._
+
+object SpiewakBintrayPlugin extends AutoPlugin {
+
+  override def requires = SpiewakPlugin && Sonatype
+
+  override def trigger = allRequirements
+
+  override def buildSettings =
+    addCommandAlias("release", "; reload; +mimaReportBinaryIssues; +publishSigned; sonatypeReleaseAll")
+
+  override def projectSettings = Seq(
+    publishMavenStyle := !sbtPlugin.value,
+
+    sonatypeProfileName := "com.codecommit",
+
+    publishTo := Some(
+      if (isSnapshot.value)
+        Opts.resolver.sonatypeSnapshots
+      else
+        Opts.resolver.sonatypeStaging))
+}
