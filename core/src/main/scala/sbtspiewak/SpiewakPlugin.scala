8aea27308b7d9dac0588e7fe17b3ba0975c3a062
Daniel Spiewak <djspiewak@gmail.com>
2018-09-06 17:51:24 -0600

    Updated kind-projector

--- core/src/main/scala/sbtspiewak/SpiewakPlugin.scala
+++ core/src/main/scala/sbtspiewak/SpiewakPlugin.scala
@@ -111,7 +111,7 @@ object SpiewakPlugin extends AutoPlugin {
       git.gitUncommittedChanges := Try("git status -s".!!.trim.length > 0).getOrElse(true))
 
   override def projectSettings = AutomateHeaderPlugin.projectSettings ++ Seq(
-    addCompilerPlugin("org.spire-math" % "kind-projector" % "0.9.6" cross CrossVersion.binary),
+    addCompilerPlugin("org.spire-math" % "kind-projector" % "0.9.7" cross CrossVersion.binary),
 
     // Adapted from Rob Norris' post at https://tpolecat.github.io/2014/04/11/scalac-flags.html
     scalacOptions ++= Seq(
