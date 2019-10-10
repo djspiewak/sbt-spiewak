df2f1868abbaae8d075e471856c26a8138949190
Daniel Spiewak <djspiewak@gmail.com>
2019-10-10 15:36:27 -0600

    Okay forgot to change this

--- core/src/main/scala/sbtspiewak/SpiewakPlugin.scala
+++ core/src/main/scala/sbtspiewak/SpiewakPlugin.scala
@@ -116,7 +116,7 @@ object SpiewakPlugin extends AutoPlugin {
       git.gitUncommittedChanges := Try("git status -s".!!.trim.length > 0).getOrElse(true))
 
   override def projectSettings = AutomateHeaderPlugin.projectSettings ++ Seq(
-    addCompilerPlugin("org.typelevel" % "kind-projector" % "0.11.0" cross CrossVersion.binary),
+    addCompilerPlugin("org.typelevel" % "kind-projector" % "0.11.0" cross CrossVersion.full),
 
     // Adapted from Rob Norris' post at https://tpolecat.github.io/2014/04/11/scalac-flags.html
     scalacOptions ++= Seq(
